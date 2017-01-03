80dd73b68188ba9c1e591aa6e721c77fe87735e7
Anna Holmgren <anna.holmgren@wigetmedia.com>
2017-01-03 10:26:00 +0100

    Added log rotation. Flask/werkzeug still logs to console as well, though :(

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,22 @@ from flask_sqlalchemy import SQLAlchemy
 from config import basedir
 from app import custom_filters
 
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
 app = Flask(__name__,
             template_folder=os.path.join(basedir, 'app', 'templates'),
             static_folder=os.path.join(basedir, 'app', 'static'))
 app.config.from_object('config')
 app.register_blueprint(custom_filters.blueprint)
+
+file_handler = TimedRotatingFileHandler(app.config['ACCESS_LOG'], when='d')
+logger = logging.getLogger('werkzeug')
+logger.handlers = [file_handler]
+logger.propagate = False
+
+app.logger.handlers = [file_handler]
+app.logger.propagate = False
 
 jsglue = JSGlue(app)
 
